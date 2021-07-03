d1de3960488c030085ab1c8e41720852e19fbbfe
Koenn <koenn@bits.team>
2021-07-03 11:36:21 +0200

    Open and close the database connection when the server starts/stops

--- src/main/java/team/bits/vanilla/fabric/BitsVanilla.java
+++ src/main/java/team/bits/vanilla/fabric/BitsVanilla.java
@@ -4,17 +4,19 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.platform.fabric.FabricServerAudiences;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandOutput;
 import org.jetbrains.annotations.NotNull;
 import team.bits.vanilla.fabric.commands.Commands;
+import team.bits.vanilla.fabric.database.driver.DatabaseConnection;
 import team.bits.vanilla.fabric.event.damage.PlayerDamageCallback;
 import team.bits.vanilla.fabric.event.sleep.PlayerMoveCallback;
 import team.bits.vanilla.fabric.event.sleep.PlayerSleepCallback;
 import team.bits.vanilla.fabric.event.sleep.PlayerWakeUpCallback;
-import team.bits.vanilla.fabric.teleport.Teleporter;
 import team.bits.vanilla.fabric.listeners.SleepListener;
+import team.bits.vanilla.fabric.teleport.Teleporter;
 
-public class BitsVanilla implements ModInitializer {
+public class BitsVanilla implements ModInitializer, ServerLifecycleEvents.ServerStopped {
 
     private static FabricServerAudiences adventure;
 
@@ -34,6 +36,8 @@ public class BitsVanilla implements ModInitializer {
         ServerLifecycleEvents.SERVER_STARTING.register(server -> adventure = FabricServerAudiences.of(server));
         ServerLifecycleEvents.SERVER_STOPPED.register(server -> adventure = null);
 
+        DatabaseConnection.open();
+
         Commands.registerCommands();
 
         SleepListener sleepListener = new SleepListener();
@@ -43,5 +47,12 @@ public class BitsVanilla implements ModInitializer {
         Teleporter teleporter = new Teleporter();
         PlayerMoveCallback.EVENT.register(teleporter);
         PlayerDamageCallback.EVENT.register(teleporter);
+
+        ServerLifecycleEvents.SERVER_STOPPED.register(this);
+    }
+
+    @Override
+    public void onServerStopped(MinecraftServer server) {
+        DatabaseConnection.close();
     }
 }
