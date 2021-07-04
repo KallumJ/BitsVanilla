769d00e5b2d781aeb8b2ac06b75632cc96a18db9
Koenn <koenn@bits.team>
2021-07-04 11:50:50 +0200

    Remove join & leave messages (we handle them in Velocity)

--- src/main/java/team/bits/vanilla/fabric/mixin/PlayerJoinMixin.java
+++ src/main/java/team/bits/vanilla/fabric/mixin/PlayerJoinMixin.java
@@ -1,13 +1,18 @@
 package team.bits.vanilla.fabric.mixin;
 
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.MessageType;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import team.bits.vanilla.fabric.event.misc.PlayerConnectEvent;
+
+import java.util.UUID;
 
 @Mixin(PlayerManager.class)
 public class PlayerJoinMixin {
@@ -16,10 +21,21 @@ public class PlayerJoinMixin {
             method = "onPlayerConnect",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"
+                    target = "Lnet/minecraft/server/world/ServerWorld;onPlayerConnected(Lnet/minecraft/server/network/ServerPlayerEntity;)V"
             )
     )
     public void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
         PlayerConnectEvent.EVENT.invoker().onPlayerConnect(player, connection);
+    }
+
+    @Redirect(
+            method = "onPlayerConnect",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"
+            )
+    )
+    public void customBroadcastChatMessage(PlayerManager playerManager, Text message, MessageType type, UUID sender) {
+        // we handle join messages in Velocity, so we want to drop this broadcast
     }
 }
---
+++ src/main/java/team/bits/vanilla/fabric/mixin/PlayerLeaveMixin.java
@@ -0,0 +1,26 @@
+package team.bits.vanilla.fabric.mixin;
+
+import net.minecraft.network.MessageType;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.UUID;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class PlayerLeaveMixin {
+
+    @Redirect(
+            method = "onDisconnected",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"
+            )
+    )
+    public void customBroadcastChatMessage(PlayerManager playerManager, Text message, MessageType type, UUID sender) {
+        // we handle leave messages in Velocity, so we want to drop this broadcast
+    }
+}
