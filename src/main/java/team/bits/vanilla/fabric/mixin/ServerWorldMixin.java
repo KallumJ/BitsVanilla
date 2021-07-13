9c40fa2f501df9ffd6dbaa09bf880d6eebc76962
Kallum <kallummj071001@gmail.com>
2021-07-13 12:42:12 +0100

    - Prevent action bar message being sent when player sleeps

---
+++ src/main/java/team/bits/vanilla/fabric/mixin/ServerWorldMixin.java
@@ -0,0 +1,23 @@
+package team.bits.vanilla.fabric.mixin;
+
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerWorld.class)
+public class ServerWorldMixin {
+
+    @Inject(
+            at = @At(value = "INVOKE",
+                    target = "Ljava/util/List;iterator()Ljava/util/Iterator;"),
+            method = "handleSleeping",
+            cancellable = true
+    )
+    public void onSleepHandle(CallbackInfo ci) {
+        // Just before Minecraft iterates through all the players and sends a message to their action bar,
+        // return out of this method
+        ci.cancel();
+    }
+}
